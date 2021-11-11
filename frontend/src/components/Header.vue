fd6a682822327ee67e79260224a04dbf0ae19d53
Noah Sanchez <alu0101134956@ull.edu.es>
2021-11-11 19:09:30 +0000

    Creando Header y añadiendo logo

---
+++ frontend/src/components/Header.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="container">
+    <div class="bigLogo">
+      <img alt="Logo" src="../assets/Logo.png"> 
+      <div class="textLogo"> 
+        <span class="bold">StackParts</span>
+        <br>
+        <span class="rainbow">Make it Real</span>
+      </div>
+    </div>
+    <div class="buttons"> 
+      <div class="login"></div>
+      <div class="register"></div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Header',
+}
+</script>
+
+<style scoped>
+.container {
+  width: 95%;
+  margin: auto;
+}
+
+.bigLogo {
+  border: 3px solid purple;
+  height: 80px;
+}
+
+.bigLogo img{
+  float: left;
+  text-align: left;
+  width: 100px;
+  height: 100%;
+}
+
+.textLogo {
+  text-align: left;
+}
+
+.bold {
+  font-weight: bold;
+  font-size: 35px;
+}
+
+.rainbow {
+  font-size: 20px;
+}
+</style>
