d1ba0175fd5054d5d60a60f9383a6ceb9e5480d3
Yeixon Morales Gonzalez <alu0101133324@ull.edu.es>
2021-12-02 14:46:58 +0000

    Inciando entorno para test

--- backend/src/database.js
+++ backend/src/database.js
@@ -1,7 +1,14 @@
 import mongoose from 'mongoose'
 import password from './pass-db';
 
-const connectionString = `mongodb+srv://StackParts:${password}@stackpartsdb.0htt5.mongodb.net/StackParts?retryWrites=true&w=majority`;
+const {NODE_ENV} = process.env
+let connectionString;
+
+if(NODE_ENV === "test") {
+  connectionString  = `mongodb+srv://StackParts:${password}@stackpartsdb.0htt5.mongodb.net/StackPartsTest?retryWrites=true&w=majority`;
+} else {
+  connectionString = `mongodb+srv://StackParts:${password}@stackpartsdb.0htt5.mongodb.net/StackParts?retryWrites=true&w=majority`;
+}
 
 mongoose.connect(connectionString, {
   useNewUrlParser : true,
