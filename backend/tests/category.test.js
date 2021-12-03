b6d0161e3e6bd7be4c90c997cf1f6e0bcb27eaa5
Yeixon Morales Gonzalez <alu0101133324@ull.edu.es>
2021-12-03 20:26:42 +0000

    Test Categorias Delete

--- backend/tests/category.test.js
+++ backend/tests/category.test.js
@@ -3,8 +3,11 @@ const supertest = require('supertest')
 const {app, server} = require('../src/index')
 
 import Category from '../src/models/category.models';
+import User from '../src/models/user.models';
 
 const api = supertest(app)
+
+let tokenAdmin
 
 const initCategory = [
   {
@@ -19,9 +22,22 @@ const initCategory = [
 
 beforeEach(async () => {
   await Category.deleteMany({})
-
   await new Category(initCategory[0]).save()
   await new Category(initCategory[1]).save()
+  
+  await User.deleteMany({})
+  const userData = {
+    "email": "admin@test.com",
+    "password": "test12345",
+    "name": "Admin",
+    "surname": "Test",
+    "role": "admin"
+  }
+  const response = await api
+    .post("/auth/signup")
+    .send(userData)
+
+  tokenAdmin = response.body.token
 })
 
 // Gets
@@ -45,17 +61,6 @@ test('The first Cateogry', async () => {
 
 // Posts
 test("A valid category can be added", async () => {
-  
-  const userData = {
-    "email": "admin@test.com",
-    "password": "test12345"
-  }
-  let tmp_token = await api
-    .post("/auth/signin")
-    .send(userData)
-
-  const token = tmp_token.body.token
-
   // Creamos una categoria
   const newCategory = {
     name: "CategoriaC",
@@ -65,17 +70,30 @@ test("A valid category can be added", async () => {
   // Hacemos el post con la categoria
   await api
     .post("/category")
-    .set('x-access-token', token)
+    .set('x-access-token', tokenAdmin)
     .send(newCategory)
     .expect(201)
     .expect('Content-Type', /application\/json/);
   
-
   // Comprobamos que la categoria se haya añadido
   const response =  await api.get('/category');
   const names = response.body.map(category => category.name)
   expect(response.body).toHaveLength(initCategory.length + 1) 
   expect(names).toContain(newCategory.name.toUpperCase())
+})
+
+//Delete
+test("Delete one Category", async () => {
+  const response =  await api.get('/category');
+  const deleteCategoryId = response.body[0].id
+  await api
+    .delete(`/category/${deleteCategoryId}`)
+    .set('x-access-token', tokenAdmin)
+    .expect(200)
+    .expect('Content-Type', /application\/json/);
+    
+  const response_ =  await api.get('/category');
+  expect(response_.body).toHaveLength(initCategory.length -1) 
 })
 
 afterAll(() => {
